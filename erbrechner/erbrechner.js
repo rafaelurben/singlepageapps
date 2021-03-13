899cec8bbed71d946379bbe65d6f7a6f40f7d850
rafaelurben <rafael.urben@gmail.com>
2021-03-13 15:57:23 +0100

    Erbrechner (calculation done)

--- erbrechner/erbrechner.js
+++ erbrechner/erbrechner.js
@@ -3,8 +3,8 @@
 ///// Calculation
 
 class Person {
-    all_people = [];
-    money_to_state = false;
+    static everyone = [];
+    static money_to_state = false;
 
     constructor(name, alive) {
         this.name = String(name);
@@ -22,7 +22,7 @@ class Person {
         this.min_share_percent = 0;
         this.min_share_absolute = 0;
 
-        this.all_people.push(this);
+        Person.everyone.push(this);
     }
 
     // Properties
@@ -30,18 +30,18 @@ class Person {
     /// Parentals
 
     get isParental1Alive() {
-        for (var child of this.children) {
+        for (let child of this.children) {
             if (child.isTreeAlive) return true;
         }
         return false;
     }
 
     get isParental2Alive() {
-        return (this.parent1 != null && this.parent1.isTreeAlive) || (this.parent2 != null && this.parent2.isTreeAlive);
+        return (this.parent1 && this.parent1.isTreeAlive) || (this.parent2 && this.parent2.isTreeAlive);
     }
 
     get isParental3Alive() {
-        return (this.parent1 != null && this.parent1.isParental2Alive) || (this.parent2 != null && this.parent2.isParental2Alive)
+        return (this.parent1 && this.parent1.isParental2Alive) || (this.parent2 && this.parent2.isParental2Alive)
     }
 
     /// Helpers
@@ -51,8 +51,8 @@ class Person {
     }
 
     get childrenWithTreeAlive() {
-        var list = [];
-        for (child of this.children) {
+        let list = [];
+        for (let child of this.children) {
             if (child.isTreeAlive) {
                 list.push(child);
             }
@@ -62,42 +62,101 @@ class Person {
 
     // Methods
 
-    addChild(child) {
+    setParents(parent1, parent2=null) {
+        parent1.children.push(child);
+        this.parent1 = parent1;
+
+        if (parent2) {
+            parent2.children.push(child);
+            this.parent2 = parent2;
+        }
+    }
+
+    addChild(child, parent2=null) {
         this.children.push(child);
+        child.parent1 = this;
+        
+        if (parent2) {
+            parent2.children.push(child);
+            child.parent2 = parent2;
+        };
+    }
+
+    /// Distribution
+
+    distributeToParental1(percent, mandatorypart = 0, ignorealive = false) {
+        if (this.alive && !ignorealive) {
+            this.share_percent += percent;
+            this.min_share_percent += percent * mandatorypart;
+        } else {
+            let people_to_share_with = this.childrenWithTreeAlive;
+            let percent_per_person = percent / people_to_share_with.length;
+            for (let person of people_to_share_with) {
+                person.distributeToParental1(percent_per_person, mandatorypart);
+            }
+        }
+    }
+
+    distributeToParental2(percent, mandatorypart = 0) {
+        let p1 = this.parent1 && this.parent1.isTreeAlive;
+        let p2 = this.parent2 && this.parent2.isTreeAlive;
+
+        if (p1 && p2) {
+            this.parent1.distributeToParental1(percent / 2);
+            this.parent2.distributeToParental1(percent / 2);
+
+            if (this.parent1.alive) this.parent1.min_share_percent = (percent / 2) * mandatorypart;
+            if (this.parent2.alive) this.parent2.min_share_percent = (percent / 2) * mandatorypart;
+        } else if (p1) {
+            this.parent1.distributeToParental1(percent);
+
+            if (this.parent1.alive) this.parent1.min_share_percent = percent * mandatorypart;
+        } else if (p2) {
+            this.parent2.distributeToParental1(percent);
+
+            if (this.parent2.alive) this.parent2.min_share_percent = percent * mandatorypart;
+        }
+    }
+
+    distributeToParental3(percent) {
+        let p1 = (this.parent1 && (this.parent1.parent1 || this.parent1.parent2));
+        let p2 = (this.parent2 && (this.parent2.parent1 || this.parent2.parent2));
+
+        if (p1 && p2) {
+            this.parent1.distributeToParental2(percent / 2, 0);
+            this.parent2.distributeToParental2(percent / 2, 0);
+        } else if (p1) {
+            this.parent1.distributeToParental2(percent, 0);
+        } else if (p2) {
+            this.parent2.distributeToParental2(percent, 0);
+        }
     }
 
     distribute() {
-        if (this.partner != null && this.partner.alive) {
+        if (this.partner && this.partner.alive) {
             if (this.isParental1Alive) {
                 this.partner.share_percent = 1/2;
                 this.partner.min_share_percent = (1/2)*(1/2);
 
-                this.subDistribute(1/2, mandatorypart=3/4, ignorealive=true);
+                this.distributeToParental1(1/2, 3/4, true);
             } else if (this.isParental2Alive) {
                 this.partner.share_percent = 3/4;
                 this.partner.min_share_percent = (3/4)*(1/2);
-                
 
+                this.distributeToParental2(1/4, 1/2)
             } else {
                 this.partner.share_percent = 1/1;
                 this.partner.min_share_percent = (1/1)*(1/2);
-
-
             }
         } else {
-
-        }
-    }
-
-    subDistribute(percent, mandatorypart=0, ignorealive=false) {
-        if (this.alive && !ignorealive) {
-            this.share_percent = percent;
-            this.min_share_percent = percent*mandatorypart;
-        } else {
-            people_to_share_with = this.childrenWithTreeAlive;
-            percent_per_person = percent / people_to_share_with.length;
-            for (person of people_to_share_with) {
-                person.subDistribute(percent_per_person, mandatorypart);
+            if (this.isParental1Alive) {
+                this.distributeToParental1(1/1, 1/2, true);
+            } else if (this.isParental2Alive) {
+                this.distributeToParental2(1/1);
+            } else if (this.isParental3Alive) {
+                this.distributeToParental3(1/1);
+            } else {
+                Person.money_to_state = true;
             }
         }
     }
@@ -105,13 +164,21 @@ class Person {
 
 ///// Interface
 
-p = new Person("MAIN", true)
-p.parent1 = new Person("Father", true)
-p.parent1.parent1 = new Person("Grandfather 1", false)
-p.parent1.parent2 = new Person("Grandmother 1", false)
-p.parent1.parent2.addChild(new Person("Uncle 1", true));
-p.parent2 = new Person("Mother", false)
-p.parent2.parent1 = new Person("Grandfather 2", false)
-p.parent2.parent2 = new Person("Grandmother 2", false)
-p.addChild(new Person("Child 1", true))
-p.addChild(new Person("Child 1", true))
+///// Tests
+
+// p = new Person("MAIN", false)
+// p.partner = new Person("Partner", false)
+
+// p.parent1 = new Person("Father", false)
+// p.parent1.parent1 = new Person("Grandfather 1", false)
+// p.parent1.parent2 = new Person("Grandmother 1", true)
+
+// p.parent2 = new Person("Mother", false)
+// p.parent2.parent1 = new Person("Grandfather 2", true)
+// p.parent2.parent2 = new Person("Grandmother 2", true)
+
+// p.parent1.addChild(new Person("Sister 1", true), p.parent2)
+// p.parent1.addChild(new Person("Sister 2", false))
+
+
+// p.distribute()
